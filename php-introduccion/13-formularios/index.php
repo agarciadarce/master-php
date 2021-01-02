326f2ff06c3b2f9d0135926c182112f693a9b5d7
Axel Eleazar García Darce <agarciadarce@gmail.com>
2021-01-02 11:25:05 -0600

    :zap: formularios

---
+++ php-introduccion/13-formularios/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formularios PHP y HTML</title>
+</head>
+<body>
+    <h1>Formulario</h1>
+    <form action="" method="POST" enctype="multipart/form-data">
+    <label for="nombre">Nombre: </label>    
+    <input type="text" name="nombre" id="nombre">
+    <br>
+    <label for="apellido">Apellido: </label>
+    <input type="text" name="apellido" id="apellido">
+    <br>
+    <input type="submit" value="Enviar">
+
+
+    </form>
+</body>
+</html>
